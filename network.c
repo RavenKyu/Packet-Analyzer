e91942d8935823b57e413a73837749594608b68d
Raven <hong18s@gmail.com>
2012-10-25 16:55:24 +0900

    랜 카드 종류를 출력 * 현재 사용중인 랜 카드의 종류를 알맞게 출력한다.

--- network.c
+++ network.c
@@ -8,78 +8,128 @@ char errbuf[PCAP_ERRBUF_SIZE];
 
 int main()
 {
-	char *nic_name;
-	pcap_t *nicdev;         /* 장치 변수 */
+    char *nic_name;
+    pcap_t *nicdev;         /* 장치 변수 */
 
-	//unsigned char *buff[1400] = {0, }; 
-	const unsigned char *uc_data;
+    //unsigned char *buff[1400] = {0, }; 
+    const unsigned char *uc_data;
 
-	struct pcap_pkthdr info;
-        struct ether_header *st_Ether;
+    struct pcap_pkthdr info;
+    struct ether_header *st_Ether;
 
-	nic_name = pcap_lookupdev(errbuf); /* 장치명을 가져온다. */
-	if(nic_name == NULL)                /* 장치명 가쳐오기를 실패할 경우 에러 메세지 처리 후 종료 */
-	{
-		printf("Failed to get the device name :: %s \n", errbuf);
-		return 0;
-	}
+    nic_name = pcap_lookupdev(errbuf); /* 장치명을 가져온다. */
+    if(nic_name == NULL)                
+    {
+        printf("Device Error\n");
+        return 0;
+    }
 
-	nicdev = pcap_open_live(nic_name, 1400, 1, 0, errbuf); /* 장치를 연다 , 장치명, 가져올 패킷 길이, 1로 해주어야 아무 패킷이나 다 받아온다.*/
-	if(nicdev == NULL)                                     /* 장치 열기를 실패했을 경우 error 메세지를 출력 후 종료 */
-	{
-		printf("The device open error :: %s \n", errbuf);
+    nicdev = pcap_open_live(nic_name, 1400, 1, 0, errbuf); /* 장치를 연다 , 장치 가져올 패킷 길이, 1로 해주어야 아무 패킷이나 다 받아온다.*/
+    if(nicdev == NULL)                                     /* 장치 열기를 실패했을 경우 error 메세지를 출력 후 종료 */
+    {
+        printf("The device open error :: %s \n", errbuf);
+            
+        return 0;
+    }
 
-		return 0;
-	}
+    printf("%s\n", nic_name);
 
-	printf("%s\n", nic_name);
+    uc_data = pcap_next(nicdev, &info);
 
-	uc_data = pcap_next(nicdev, &info);
+    hex_viewer((unsigned char *)uc_data, 10);
 
-	hex_viewer((unsigned char *)uc_data, 10);
+    /* 랜카드 종류를 출력한다. */
+    switch(pcap_datalink(nicdev))
+    {
+    case 0:
+        printf("no link-layer encapsulation\n");
+        break;
+                                
+    case 1:
+        printf("Ethernet (10Mb)\n");
+        break;
 
-        /* MAC Address를 출력한다. */
-        st_Ether = (struct ether_header *)uc_data;
-	printf("MAC [%02X:%02X:%02X:%02X:%02X:%02X]" 
-			" <- [%02X:%02X:%02X:%02X:%02X:%02X] \n", 
-               st_Ether -> ether_dhost[0], /* 목적지 MAC Address */
-               st_Ether -> ether_dhost[1],
-               st_Ether -> ether_dhost[2],
-               st_Ether -> ether_dhost[3],
-               st_Ether -> ether_dhost[4],
-               st_Ether -> ether_dhost[5],
-               st_Ether -> ether_shost[0], /* 출발지 MAC Address */
-               st_Ether -> ether_shost[1],
-               st_Ether -> ether_shost[2],
-               st_Ether -> ether_shost[3],
-               st_Ether -> ether_shost[4],
-               st_Ether -> ether_shost[5]
-            );
+    case 2:
+        printf("Experimental Ethernet (3Mb)\n");
+        break;
+
+    case 3:
+        printf("Amateur Radio AX.25\n");
+        break;
+
+    case 4:
+        printf("Proteon ProNET Token Ring\n");
+        break;
+
+    case 5:
+        printf("Chaos\n");
+        break;
+
+    case 6:
+        printf("IEEE 802 Networks\n");
+        break;
+
+    case 7:
+        printf("ARCNET\n");
+        break;
+
+    case 8:
+        printf("Serial Line IP\n");
+        break;
+
+    case 9:
+        printf("Point-to-point Protocol\n");
+        break;
+
+    case 10:
+        printf("FDDI\n");
+        break;
+            
+    }
+
+    st_Ether = (struct ether_header *)uc_data;
+
+    /* MAC Address를 출력한다. */
+    printf("MAC [%02X:%02X:%02X:%02X:%02X:%02X]" 
+           " <- [%02X:%02X:%02X:%02X:%02X:%02X] \n", 
+           st_Ether -> ether_dhost[0], /* 목적지 MAC Address */
+           st_Ether -> ether_dhost[1],
+           st_Ether -> ether_dhost[2],
+           st_Ether -> ether_dhost[3],
+           st_Ether -> ether_dhost[4],
+           st_Ether -> ether_dhost[5],
+           st_Ether -> ether_shost[0], /* 출발지 MAC Address */
+           st_Ether -> ether_shost[1],
+           st_Ether -> ether_shost[2],
+           st_Ether -> ether_shost[3],
+           st_Ether -> ether_shost[4],
+           st_Ether -> ether_shost[5]
+        );
         
-        switch(ntohs(st_Ether -> ether_type))
-        {
-        case ETHERTYPE_PUP:
-            printf("Xerox PUP\n");
-            break;
+    switch(ntohs(st_Ether -> ether_type))
+    {
+    case ETHERTYPE_PUP:
+        printf("Xerox PUP\n");
+        break;
             
-        case ETHERTYPE_IP:
-            printf("IP\n");
-            break;
+    case ETHERTYPE_IP:
+        printf("IP\n");
+        break;
             
-        case ETHERTYPE_ARP:
-            printf("Address resolution\n");
-            break;
+    case ETHERTYPE_ARP:
+        printf("Address resolution\n");
+        break;
             
-        case ETHERTYPE_REVARP:
-            printf("Reverse ARP\n");
-            break;
+    case ETHERTYPE_REVARP:
+        printf("Reverse ARP\n");
+        break;
             
-        default:
-            printf("Unknown Type\n");
-            /* 패킷의 종류를 출력 */
-            /* printf("%04X\n", ntohs(st_Ether -> ether_type)); /\* 호스트 형태로 바꾸겠다. *\/ */
-        }
+    default:
+        printf("Unknown Type\n");
+        /* 패킷의 종류를 출력 */
+        /* printf("%04X\n", ntohs(st_Ether -> ether_type)); /\* 호스트 형태로 바꾸겠다. *\/ */
+    }
                 
-	pcap_close(nicdev);		
-	return 0;
+    pcap_close(nicdev);		
+    return 0;
 }
