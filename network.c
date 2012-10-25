f346cf179c103d3dde6a892c51e7be1075aa7957
Raven <hong18s@gmail.com>
2012-10-25 13:33:43 +0900

    MAC Address 를 출력한다. * 주소를 이용하여 출력한다.

--- network.c
+++ network.c
@@ -7,29 +7,50 @@ char errbuf[PCAP_ERRBUF_SIZE];
 int main()
 {
 	char *nic_name;
-        pcap_t *nicdev;         /* 장치 변수 */
+	pcap_t *nicdev;         /* 장치 변수 */
 
-        unsigned char *buff[1400] = {0, };
+	//unsigned char *buff[1400] = {0, }; 
+	const unsigned char *uc_data;
 
-        nic_name = pcap_lookupdev(errbuf); /* 장치명을 가져온다. */
-        if(nic_name = NULL)                /* 장치명 가쳐오기를 실패할 경우 에러 메세지 처리 후 종료 */
-        {
-            printf("Failed to get the device name :: %s \n", errbuf);
-            return 0;
-        }
-        
-        nicdev = pcap_open_live(nic_name, 1400, 1, 0, errbuf); /* 장치를 연다 , 장치명, 가져올 패킷 길이, 1로 해주어야 아무 패킷이나 다 받아온다.*/
-        if(nicdev == NULL)                                     /* 장치 열기를 실패했을 경우 error 메세지를 출력 후 종료 */
-        {
-            printf("The device open error :: %s \n", errbuf);
+	struct pcap_pkthdr info;  
 
-            return 0;
-        }
-        
-        hex_viewer(buff, 20);   /* 버퍼의 내용을 Hex_viewer 로 출력한다. */
-        
+	nic_name = pcap_lookupdev(errbuf); /* 장치명을 가져온다. */
+	if(nic_name == NULL)                /* 장치명 가쳐오기를 실패할 경우 에러 메세지 처리 후 종료 */
+	{
+		printf("Failed to get the device name :: %s \n", errbuf);
+		return 0;
+	}
+
+	nicdev = pcap_open_live(nic_name, 1400, 1, 0, errbuf); /* 장치를 연다 , 장치명, 가져올 패킷 길이, 1로 해주어야 아무 패킷이나 다 받아온다.*/
+	if(nicdev == NULL)                                     /* 장치 열기를 실패했을 경우 error 메세지를 출력 후 종료 */
+	{
+		printf("The device open error :: %s \n", errbuf);
+
+		return 0;
+	}
+
 	printf("%s\n", nic_name);
 
-        pcap_close(nicdev);		
+	uc_data = pcap_next(nicdev, &info);
+
+	hex_viewer((unsigned char *)uc_data, 10);
+
+        /* MAC Address를 출력한다. */
+	printf("MAC [%02X:%02X:%02X:%02X:%02X:%02X]" 
+			" <- [%02X:%02X:%02X:%02X:%02X:%02X] \n", 
+               *(uc_data + 0),
+               *(uc_data + 1),
+               *(uc_data + 2),
+               *(uc_data + 3),
+               *(uc_data + 4),
+               *(uc_data + 5),
+               *(uc_data + 6),
+               *(uc_data + 7),
+               *(uc_data + 8),
+               *(uc_data + 9),
+               *(uc_data + 10),
+               *(uc_data + 11));
+
+	pcap_close(nicdev);		
 	return 0;
 }
