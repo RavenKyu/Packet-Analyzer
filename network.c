6f5f8945f73c468867a9b415fb80bea4015cbe4a
Raven <hong18s@gmail.com>
2012-10-25 15:25:55 +0900

    Packet Header 구조체를 이용하여 MAC Address 출력 * Packet Header의 정보를 명시하고 있는 ethernet.h 를 include 했다. * Header에서 명시한 구조체를 참고하여 MAC Address를 출력한다.

--- network.c
+++ network.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <pcap/pcap.h>          /* man page에서 가리키는 위치가 잘못 적혀 있을 수도 있다. */
+#include <net/ethernet.h>       /* 패킷의 구조체를 명시 해 두었다. */
+
 #include "hex_viewer.h"
 
 char errbuf[PCAP_ERRBUF_SIZE];
@@ -12,7 +14,8 @@ int main()
 	//unsigned char *buff[1400] = {0, }; 
 	const unsigned char *uc_data;
 
-	struct pcap_pkthdr info;  
+	struct pcap_pkthdr info;
+        struct ether_header *st_Ether;
 
 	nic_name = pcap_lookupdev(errbuf); /* 장치명을 가져온다. */
 	if(nic_name == NULL)                /* 장치명 가쳐오기를 실패할 경우 에러 메세지 처리 후 종료 */
@@ -36,20 +39,22 @@ int main()
 	hex_viewer((unsigned char *)uc_data, 10);
 
         /* MAC Address를 출력한다. */
+        st_Ether = (struc ether_header *)uc_data;
 	printf("MAC [%02X:%02X:%02X:%02X:%02X:%02X]" 
 			" <- [%02X:%02X:%02X:%02X:%02X:%02X] \n", 
-               *(uc_data + 0),
-               *(uc_data + 1),
-               *(uc_data + 2),
-               *(uc_data + 3),
-               *(uc_data + 4),
-               *(uc_data + 5),
-               *(uc_data + 6),
-               *(uc_data + 7),
-               *(uc_data + 8),
-               *(uc_data + 9),
-               *(uc_data + 10),
-               *(uc_data + 11));
+               st_Ether -> ether_dhost[0], /* 목적지 MAC Address */
+               st_Ether -> ether_dhost[1],
+               st_Ether -> ether_dhost[2],
+               st_Ether -> ether_dhost[3],
+               st_Ether -> ether_dhost[4],
+               st_Ether -> ether_dhost[5],
+               st_Ether -> ether_shost[0], /* 출발지 MAC Address */
+               st_Ether -> ether_shost[1],
+               st_Ether -> ether_shost[2],
+               st_Ether -> ether_shost[3],
+               st_Ether -> ether_shost[4],
+               st_Ether -> ether_shost[5]
+            );
         
         /* IP 주소를 출력한다. */
         if(0x45 == *(uc_data + 14)) /* 패킷을 받았을 때만 출력한다. */
